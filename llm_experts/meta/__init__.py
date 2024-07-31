7fa588617fb5c69847b8074f9b371defd6d7d561
Bas <bastiansg.bas@gmail.com>
2024-07-31 16:12:20 -0300

    upgrade common

--- llm_experts/meta/__init__.py
+++ llm_experts/meta/__init__.py
@@ -20,4 +20,7 @@ from .data_models.experts.generic_assistant import (  # noqa
 )
 
 from .data_models.store.mongo_store import StoreMessages  # noqa
-from .interfaces.openai_chat_expert import OpenAIChatExpert, InMemoryHistory
+from .interfaces.openai_chat_expert import (  # noqa
+    OpenAIChatExpert,
+    InMemoryHistory,
+)
